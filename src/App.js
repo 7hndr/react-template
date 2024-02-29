1501d32c91c310abacfa1c703ff515e00914170d
thndr <thndr.hell@gmail.com>
2024-02-29 18:43:50 +0400

    main. jsx template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './images/logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img
+        src={logo}
+        className="App-logo"
+        alt="logo"
+      />
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer">
+        React docs
+      </a>
+      {/* partially declarative below */}
+      <span className="App-copyright">{`All rights reserved | ${new Date().getFullYear()}`}</span>
+    </header>
+  </div>
+)
