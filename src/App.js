f069829a6631cd167170bf7eabe76a7e8427d02b
thndr <thndr.hell@gmail.com>
2024-02-29 19:00:57 +0400

    jsx-template. jsx version

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
-import { createElement as el } from 'react' // declarative
 import logo from './images/logo.svg'
 import './App.css'
 
-export const App = () => {
-  // declarative below
-  const currentYear = new Date().getFullYear()
-
-  return el(
-    'div',
-    { className: 'App' },
-    el(
-      'header',
-      { className: 'App-header' },
-      el('img', { className: 'App-logo', alt: 'logo', src: logo }),
-      el(
-        'a',
-        {
-          className: 'App-link',
-          href: 'https://reactjs.org',
-          target: '_blank',
-          rel: 'noopener noreferrer'
-        },
-        'React docs'
-      ),
-      el(
-        'span',
-        {
-          className: 'App-copyright'
-        },
-        `All rights reserved | ${currentYear}`
-      )
-    )
-  )
-}
+export const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img
+        src={logo}
+        className="App-logo"
+        alt="logo"
+      />
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer">
+        React docs
+      </a>
+      {/* partially imperative  below */}
+      <span className="App-copyright">{`All rights reserved | ${new Date().getFullYear()}`}</span>
+    </header>
+  </div>
+)
